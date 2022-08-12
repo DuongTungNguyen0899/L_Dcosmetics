4297ccd2c143f6a68350b3fa64541cf5d7c0bffc
DuongTungNguyen0899 <Duongtn0899@gmail.com>
2022-08-12 16:40:16 +0700

    project Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './inc-component/footer/footer.component';
+import { HeaderComponent } from './inc-component/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MainComponent } from './inc-component/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
